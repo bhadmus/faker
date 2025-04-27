e7daa551d0e19ce1fef40e4435235c5f97c6b782
Ademola Bhadmus <sanguine@Ademolas-MacBook-Pro.local>
2025-04-27 20:22:46 +0100

    added more nigeria network format

--- src/locales/en_NG/phone_number/format/international.ts
+++ src/locales/en_NG/phone_number/format/international.ts
@@ -1,7 +1,35 @@
 export default [
-  '+234803#######',
-  '+234703#######',
-  '+234809#######',
-  '+234802#######',
-  '+234805#######',
+  '+234803#######', // MTN Nigeria
+  '+234703#######', // MTN Nigeria
+  '+234708#######', // Airtel Nigeria
+  '+234701#######', // Airtel Nigeria
+  '+234809#######', // 9mobile 
+  '+234802#######', // Airtel Nigeria
+  '+234805#######', // Globacom
+  '+234806#######', // MTN Nigeria
+  '+234807#######', // Globacom
+  '+234808#######', // Airtel Nigeria
+  '+234809#######', // 9mobile
+  '+234811#######', // Globacom
+  '+234812#######', // Airtel Nigeria
+  '+234813#######', // MTN Nigeria
+  '+234814#######', // MTN Nigeria
+  '+234815#######', // Globacom
+  '+234816#######', // MTN Nigeria
+  '+234817#######', // 9mobile
+  '+234818#######', // 9mobile
+  '+234901#######', // Airtel Nigeria
+  '+234902#######', // Airtel Nigeria
+  '+234903#######', // MTN Nigeria
+  '+234904#######', // Airtel Nigeria
+  '+234905#######', // Globacom
+  '+234906#######', // MTN Nigeria
+  '+234907#######', // Airtel Nigeria
+  '+234908#######', // 9mobile
+  '+234909#######', // 9mobile
+  '+234911#######', // Airtel Nigeria
+  '+234912#######', // Airtel Nigeria
+  '+234913#######', // MTN Nigeria
+  '+234915#######', // Globacom
+  '+234916#######', // MTN Nigeria
 ];
--- src/locales/en_NG/phone_number/format/national.ts
+++ src/locales/en_NG/phone_number/format/national.ts
@@ -1,7 +1,35 @@
 export default [
-  '0803 ### ####',
-  '0703 ### ####',
-  '0809 ### ####',
-  '0802 ### ####',
-  '0805 ### ####',
+  '0803 ### ####', // MTN Nigeria
+  '0703 ### ####', // MTN Nigeria
+  '0708 ### ####', // Airtel Nigeria
+  '0701 ### ####', // Airtel Nigeria
+  '0809 ### ####', // 9mobile 
+  '0802 ### ####', // Airtel Nigeria
+  '0805 ### ####', // Globacom
+  '0806 ### ####', // MTN Nigeria
+  '0807 ### ####', // Globacom
+  '0808 ### ####', // Airtel Nigeria
+  '0809 ### ####', // 9mobile
+  '0811 ### ####', // Globacom
+  '0812 ### ####', // Airtel Nigeria
+  '0813 ### ####', // MTN Nigeria
+  '0814 ### ####', // MTN Nigeria
+  '0815 ### ####', // Globacom
+  '0816 ### ####', // MTN Nigeria
+  '0817 ### ####', // 9mobile
+  '0818 ### ####', // 9mobile
+  '0901 ### ####', // Airtel Nigeria
+  '0902 ### ####', // Airtel Nigeria
+  '0903 ### ####', // MTN Nigeria
+  '0904 ### ####', // Airtel Nigeria
+  '0905 ### ####', // Globacom
+  '0906 ### ####', // MTN Nigeria
+  '0907 ### ####', // Airtel Nigeria
+  '0908 ### ####', // 9mobile
+  '0909 ### ####', // 9mobile
+  '0911 ### ####', // Airtel Nigeria
+  '0912 ### ####', // Airtel Nigeria
+  '0913 ### ####', // MTN Nigeria
+  '0915 ### ####', // Globacom
+  '0916 ### ####', // MTN Nigeria
 ];
